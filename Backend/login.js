6b2853fbe18f961b39c78512571a8c599c3dfe82
Redean <redean.akunzada@gmail.com>
2023-11-07 00:09:27 +0100

    Page de login crée et fonctionelle

---
+++ Backend/login.js
@@ -0,0 +1,35 @@
+// Fonction qui permets le login utilisateur
+const boutonSeConnecter = document.getElementById("se-connecter")
+boutonSeConnecter.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const motDePasse = document.getElementById("motdepasse").value;
+
+    const userLogin = {
+        email: email,
+        password: motDePasse,
+    };
+
+    const userLoginJson = JSON.stringify(userLogin);
+
+    fetch("http://localhost:5678/api/users/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json", "accept": "application/json" },
+        body: userLoginJson
+    })
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error("Erreur de connexion");
+        }
+    })
+    .then(data => {
+        console.log("Connexion réussie:", data);
+        window.location.href = "../FrontEnd/index.html";
+    })
+    .catch(error => {
+        console.error("Erreur de connexion:", error);
+    });
+});
--- Backend/main.js
+++ Backend/main.js
@@ -2,6 +2,16 @@
 const works = await fetch("http://localhost:5678/api/works/").then(works => works.json());
 const gallery = document.querySelector(".gallery");
 
+// Affichage different des boutons actives
+const boutons = document.querySelectorAll('.filter button');
+
+boutons.forEach(bouton => {
+  bouton.addEventListener('click', function() {
+    boutons.forEach(b => b.classList.remove('active'));
+    bouton.classList.add('active');
+  });
+});
+
 // Fonction qui génére l'affichage des projets.
 function generateWorks(works) {
     for (let i = 0; i < works.length; i++) {
@@ -16,11 +26,15 @@ function generateWorks(works) {
         figure.appendChild(textImage);
         console.log(imageElement.src);
     }
-}
+};
 
 
 // Première affichage principal de la page.
-generateWorks(works);
+try {
+    generateWorks(works);
+} catch (error) {
+    console.error("Une erreur s'est produite lors de la génération des œuvres :", error);
+};
 
 
 // Affichage filtrer par catégorie.
@@ -28,25 +42,25 @@ const boutonTous = document.querySelector('.btn-tous');
 boutonTous.addEventListener("click", function() {
     document.querySelector(".gallery").innerHTML = '';
     generateWorks(works);
-})
+});
 
 const boutonObjets = document.querySelector('.btn-objets');
 boutonObjets.addEventListener("click", function() {
     document.querySelector(".gallery").innerHTML = '';
     let worksObjets = works.filter(work => work.category.name === "Objets");
     generateWorks(worksObjets);
-})
+});
 
 const boutonAppartements = document.querySelector('.btn-appartements');
 boutonAppartements.addEventListener("click", function() {
     document.querySelector(".gallery").innerHTML = '';
     let worksAppartements = works.filter(work => work.category.name === "Appartements");
     generateWorks(worksAppartements);
-})
+});
 
 const boutonHotelsRestaurants = document.querySelector('.btn-hotels-restaurants');
 boutonHotelsRestaurants.addEventListener("click", function() {
     document.querySelector(".gallery").innerHTML = '';
     let worksHotelsRestaurants = works.filter(work => work.category.name === "Hotels & restaurants");
     generateWorks(worksHotelsRestaurants);
-})
+});
